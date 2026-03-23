ca3bec142c7ab58285b55bd70a5f668a7ea250e2
chandru-byte <chandruvenkat30@gmail.com>
2026-03-23 10:24:24 +0530

    fixed port issue

--- app.py
+++ app.py
@@ -145,7 +145,7 @@ if __name__ == "__main__":
     port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port)
   
-  from flask import Flask
+from flask import Flask
 import os
 
 app = Flask(__name__)
